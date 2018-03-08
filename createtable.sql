fd4c3a83da6df60ef0f145650c2f9716d6149459
jins2 <35314760+jins2@users.noreply.github.com>
2018-03-07 23:22:07 -0800

    Add files via upload

---
+++ createtable.sql
@@ -0,0 +1,35 @@
+create database cs221;
+use cs221;
+create table `indexNum`
+(
+	`term` varchar(100) not null,
+	`docu` varchar(100) not null,
+	`score` decimal(8,4) not null
+);
+
+
+create table `indexA_F`
+(
+	`term` varchar(100) not null,
+	`docu` varchar(100) not null,
+	`score` decimal(8,4) not null
+);
+
+create table `indexG_L`
+(
+	`term` varchar(100) not null,
+	`docu` varchar(100) not null,
+	`score` decimal(8,4) not null
+);
+create table `indexM_R`
+(
+	`term` varchar(100) not null,
+	`docu` varchar(100) not null,
+	`score` decimal(8,4) not null
+);
+create table `indexS_Z`
+(
+	`term` varchar(100) not null,
+	`docu` varchar(100) not null,
+	`score` decimal(8,4) not null
+);
